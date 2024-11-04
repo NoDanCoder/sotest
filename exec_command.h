d4e26a2ef1be1d5d036dd423070495e8f93a7c38
Daniel Robayo <danielrobayo@192.168.0.12>
2024-11-04 00:11:31 -0500

    [Add] full interactive console

--- exec_command.h
+++ exec_command.h
@@ -3,6 +3,7 @@
 
 #include "load_commands.h"
 
+int validate_library(Command *use_command);
 Command *run_call_command(Metadata metadata, Command *use_command, Command *call_command);
 
 #endif
--- interactive.c
+++ interactive.c
@@ -5,6 +5,7 @@
 #include "metadata.h"
 #include "parse_command.h"
 #include "load_commands.h"
+#include "exec_command.h"
 
 void print_ps1(void)
 {
@@ -23,23 +24,53 @@ void copy_to_command(Metadata metadata, const char *line, Command *command) {
     command->content[len_line - 1] = '\0';
 }
 
+void clean_command(Command *command) {
+    command->content = NULL;
+    command->identifier = NULL;
+    command->argument = NULL;
+    command->next = NULL;
+}
+
+void transfer_command(Command *self, Command *other) {
+    other->content = self->content;
+    other->identifier = self->identifier;
+    other->argument = self->argument;
+    other->next = self->next;
+    clean_command(self);
+}
+
 void run_interactive_mode(Metadata metadata) {
     printf("Welcome to the interactive shell!\n");
     printf("Type 'help' for available commands\n\n");
 
-    Command *command = create_command_node(metadata, NULL, NULL, 0);
+    Command *use_command = create_command_node(metadata, NULL, NULL, 0);
+    Command *call_command = create_command_node(metadata, NULL, NULL, 0);
     char *line = NULL;
     size_t size = 0;
-
+    char session_started = 0;
     while (1) {
         print_ps1();
         if (getline_2001(metadata, &line, &size, stdin) == -1) {
             printf("\nReceived EOF. Exiting...\n");
             break;
         }
-        copy_to_command(metadata, line, command);
-        if (lookup_use_command(metadata, command)) {
-            printf("well done!");
+        if (!session_started) {
+            copy_to_command(metadata, line, use_command);
+            if (lookup_use_command(metadata, use_command) && validate_library(use_command)) {
+                session_started = 1;
+            }
+        } else {
+            copy_to_command(metadata, line, call_command);
+            Command *found = lookup_call_command(metadata, call_command);
+            if (found && strcmp(found->identifier, "call") == 0) {
+                run_call_command(metadata, use_command, call_command);
+            } else if (found && strcmp(found->identifier, "use") == 0) {
+                transfer_command(call_command, use_command);
+                if (!lookup_use_command(metadata, use_command)) {
+                    session_started = 0;
+                    clean_command(use_command);
+                }
+            }
         }
         printf("\n");
     }
